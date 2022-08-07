a2cfa2125dcfe1c727cd60858f3c657b2dba3a5f
jaisonrobson <xd_jrg@hotmail.com>
2022-08-07 19:09:00 -0300

    fix: removido referencia ao babel para o next devido a erro (possivelmente uma alteracao recente no pacote next causou breaking changes)

--- ecommerce/pages/index.js
+++ ecommerce/pages/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+      <div>
+        <h2>Produtos mais vendidos</h2>
+        <p>Alto-falantes mais variados</p>
+      </div>
+
+      <div>
+        {['Product 1', 'Product2'].map(
+            (product) => product
+        )}
+      </div>
+
+      Footer
+    </>
   )
 }
 
-export default index
+export default Home
