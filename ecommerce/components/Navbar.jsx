8eea81ef76de8fbb6225a5cfd2a38e8423570787
jaisonrobson <xd_jrg@hotmail.com>
2022-08-11 08:05:21 -0300

    feat: criado conteudo de leiaute para barra de navegacao do site

--- ecommerce/components/Navbar.jsx
+++ ecommerce/components/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
 
-const Navbar = () => {
+const Navbar = ({  }) => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Loja Online</Link>
+      </p>
+
+      <button
+        className='cart-icon'
+        type="button"
+        onClick=""
+      >
+        <AiOutlineShopping />
+
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
   )
 }
 
