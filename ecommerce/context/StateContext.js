8e31260b35ee15a17344f14b613753bdf784507a
jaisonrobson <xd_jrg@hotmail.com>
2022-08-18 11:00:58 -0300

    feat: criado context para envolver o projeto inteiro & utilizado na raiz do website & utilizado Toaster na raiz do website

---
+++ ecommerce/context/StateContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useContext, useState, useEffect } from 'react'
+import { toast } from 'react-hot-toast'
+
+const Context = createContext()
+
+export const StateContext = ({ children }) => {
+    const [showCart, setShowCart] = useState(false)
+    const [cartItems, setCartItems] = useState()
+    const [totalPrice, setTotalPrice] = useState()
+    const [totalQuantities, setTotalQuantities] = useState()
+    const [quantity, setQuantity] = useState(1)
+
+    return (
+        <Context.Provider
+            value={{
+                showCart,
+                cartItems,
+                totalPrice,
+                totalQuantities,
+                quantity
+            }}
+        >
+            {children}
+        </Context.Provider>
+    )
+}
--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -1,12 +1,20 @@
+import React from 'react'
+import { Toaster } from 'react-hot-toast'
+
 import '../styles/globals.css'
 
 import { Layout } from '../components'
+import { StateContext } from '../context/StateContext'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
   )
 }
 
