9fca8092eeffd32de247cab5100c3bc979425bab
jaisonrobson <xd_jrg@hotmail.com>
2022-09-03 00:40:21 -0300

    fix: ajustado exportacao de components de 'view'

--- ecommerce/components/index.js
+++ ecommerce/components/index.js
@@ -1,4 +1,4 @@
 export { MainLayout } from './layout'
 export { Image, Link } from './embraced'
 export { RoundedButton } from './minimum'
-export { Cart, Footer, FooterBanner, HeroBanner, Navbar, Product } from './views'
+export { Cart, Footer, FooterBanner, HeroBanner, Navbar, Product } from './view'
